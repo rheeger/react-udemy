b2b103eb75aa62e7034b0a0029c9d910c3dcc3b0
rheeger <rheeger@gmail.com>
2019-03-16 16:24:20 -0700

    need moar practice but this works

--- 06-blog/src/actions/index.js
+++ 06-blog/src/actions/index.js
@@ -1,17 +1,34 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+	_.chain(getState().posts).map('userId').uniq().forEach((id) => dispatch(fetchUser(id))).value();
+};
 
 export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonPlaceholder.get('/posts');
 	dispatch({
 		type: 'FETCH_POSTS',
-		payload: response
+		payload: response.data
 	});
 };
 
-export const fetchUsers = () => async (dispatch) => {
-	const response = await jsonPlaceholder.get('/users');
-	dispatch({
-		type: 'FETCH_USERS',
-		payload: response
-	});
+export const fetchUser = (id) => async (dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({ type: 'FETCH_USER', payload: response.data });
 };
+
+//MEMOIZE VERSION...only can work once, not good for things that update.
+// export const fetchUser = (id) => (dispatch) => {
+// 	_fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+// 	const response = await jsonPlaceholder.get(`/users/${id}`);
+// 	dispatch({
+// 		type: 'FETCH_USER',
+// 		payload: response.data
+// 	});
+// });
