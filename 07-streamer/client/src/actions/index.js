d2b58ebd2a4d8d7d6deece7bebe5bf3d063df1d1
rheeger <rheeger@gmail.com>
2019-03-18 17:25:19 -0700

    setting up redux forms

--- 07-streamer/client/src/actions/index.js
+++ 07-streamer/client/src/actions/index.js
@@ -3,7 +3,7 @@ import { SIGN_IN, SIGN_OUT } from './types';
 export const signIn = (userId) => {
 	return {
 		type: SIGN_IN,
-		userId: userId
+		payload: userId
 	};
 };
 export const signOut = () => {
