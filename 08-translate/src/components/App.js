bbaa5a1e5f981a6bd3c0187442b7b64b0c268367
rheeger <rheeger@gmail.com>
2019-03-20 11:46:39 -0700

    getting started with context vs. redux

---
+++ 08-translate/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+	state = { language: 'english' };
+
+	onLanguageChange = (language) => {
+		this.setState({ language });
+	};
+
+	render() {
+		return (
+			<div className="ui container">
+				<div>
+					Select a Language:
+					<i className="flag us" onClick={() => this.onLanguageChange('english')} />
+					<i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default App;
