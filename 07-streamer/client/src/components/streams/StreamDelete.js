ed3b5ab3bc053a4f761e8be16bab6bf0c6a61b0e
rheeger <rheeger@gmail.com>
2019-03-19 17:03:54 -0700

    React side done. Moving onto RMTP & Streaving Video support

--- 07-streamer/client/src/components/streams/StreamDelete.js
+++ 07-streamer/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream, deleteStream } from '../../actions';
 
-const StreamDelete = () => {
-	return <div>StreamDelete</div>;
+class StreamDelete extends React.Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
+	renderActions() {
+		return (
+			<div className="actions">
+				<Link to="/" className="ui button">
+					Cancel
+				</Link>
+				<button
+					onClick={() => this.props.deleteStream(this.props.match.params.id)}
+					className="ui button negative"
+				>
+					Terminate
+				</button>
+			</div>
+		);
+	}
+
+	renderContent() {
+		if (!this.props.stream) {
+			return `Terminate Stream:`;
+		}
+
+		return `Terminate Stream: "${this.props.stream.title}"`;
+	}
+	render() {
+		return (
+			<Modal
+				onDismiss={() => history.push(`/`)}
+				title={this.renderContent()}
+				content="Are you sure you want to end your program?"
+				actions={this.renderActions()}
+			/>
+		);
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
--- 07-streamer/client/src/components/streams/StreamShow.js
+++ 07-streamer/client/src/components/streams/StreamShow.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => {
-	return <div>StreamShow</div>;
+class StreamShow extends React.Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.props.stream.title}</h1>
+				<h5>{this.props.stream.description}</h5>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = (state, ownProps) => {
+	return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(null, { fetchStream })(StreamShow);
