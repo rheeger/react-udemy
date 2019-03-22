fcc5f246d1f9aac2c592b772aa417f63b9129403
rheeger <rheeger@gmail.com>
2019-03-22 10:13:21 -0700

    refactor complete, COURSE COMPLETE

--- 10-seasons-with-hooks/src/index.js
+++ 10-seasons-with-hooks/src/index.js
@@ -2,32 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Loading from './Loading';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-	state = { lat: null, errorMessage: '' };
+const App = () => {
+	const [ lat, errorMessage ] = useLocation();
 
-	componentDidMount() {
-		window.navigator.geolocation.getCurrentPosition(
-			(position) => this.setState({ lat: position.coords.latitude }),
-			(err) => this.setState({ errorMessage: err.message })
-		);
+	let content;
+	if (errorMessage) {
+		content = <div>Error: {errorMessage}</div>;
+	} else if (lat) {
+		content = <SeasonDisplay lat={lat} />;
+	} else {
+		content = <Loading message="trying to find yo ass..." />;
 	}
-
-	renderContent() {
-		if (this.state.errorMessage && !this.state.lat) {
-			return <div> Error: {this.state.errorMessage}</div>;
-		}
-
-		if (!this.state.errorMessage && this.state.lat) {
-			return <SeasonDisplay lat={this.state.lat} />;
-		}
-
-		return <Loading message="trying to find yo ass..." />;
-	}
-
-	render() {
-		return <div>{this.renderContent()}</div>;
-	}
-}
+	return <div>{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
