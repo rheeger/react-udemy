b294be3c5ffb141a27f0cdcdd5767ae5723d924c
rheeger <rheeger@gmail.com>
2019-03-21 23:23:37 -0700

    gotta go to sleep. will finish this before I leave tomorrow

--- 09-simple-hooks/src/components/App.js
+++ 09-simple-hooks/src/components/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
-class App extends React.Component {
-	render() {
-		return <div className="ui container">App</div>;
-	}
-}
+const App = () => {
+	const [ resource, setResource ] = useState('Posts');
+	return (
+		<div className="ui container">
+			<div>
+				<button onClick={() => setResource('Posts')}>Posts</button>
+				<button onClick={() => setResource('Todos')}>Todos</button>
+			</div>
+			<ResourceList resource={resource} />
+		</div>
+	);
+};
 
 export default App;
---
+++ 09-simple-hooks/src/components/ResourceList.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ResourceList = ({ resource }) => {
+	const [ resources, setResources ] = useState([]);
+
+	const fetchResource = async () => {
+		const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+		this.setState({ resources: response.data });
+	};
+	return <div>{resources.length}</div>;
+};
+
+export default ResourceList;
